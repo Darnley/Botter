3a7764503e9a5b83a06b9ae8c2d93fc11b49e10f
Darnley Costa <darnleycosta@gmail.com>
2017-12-04 14:00:35 -0200

    Create Bot class.

---
+++ Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Botter
+{
+    class Bot
+    {
+        public int Id { get; protected set; }
+        public string Regname { get; protected set; }
+        public string Password { get; protected set; }
+        public string DisplayName { get; protected set; }
+        public string AvatarUrl { get; protected set; }
+        public string HomepageUrl { get; protected set; }
+    }
+}
