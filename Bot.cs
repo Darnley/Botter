1822aa281f30b447a849f7a21316698b400fd526
Darnley Costa <darnleycosta@gmail.com>
2017-12-04 14:50:24 -0200

    Add Bot class construtor to create new account ID.

--- Bot.cs
+++ Bot.cs
@@ -6,12 +6,15 @@ using System.Threading.Tasks;
 
 namespace Botter
 {
+    /// <summary>
+    /// Store Bot Information.
+    /// </summary>
     class Bot
     {
         public int Id { get; protected set; }
         public string Regname { get; protected set; }
         public string Password { get; protected set; }
-        public BotAuthenticationKeys AuthenticationKeys { get; set; }
+        public BotAuthenticationKeys AuthenticationKeys { get; protected set; }
         public string DisplayName { get; protected set; }
         public string AvatarUrl { get; protected set; }
         public string HomepageUrl { get; protected set; }
@@ -19,9 +22,18 @@ namespace Botter
 
         /// <summary>
         /// Bot class constructor.
+        /// Generate a new ID.
+        /// </summary>
+        public Bot()
+        {
+
+        }
+
+        /// <summary>
+        /// Bot class constructor.
         /// </summary>
         /// <param name="botId">Bot Account ID</param>
-        public Bot(int botId)
+        public Bot(int botId, string botAuthenticationKey1, string botAuthenticationKey2)
         {
             this.Id = botId;
         }
@@ -34,7 +46,7 @@ namespace Botter
         /// <param name="botPassword">Bot Account Password</param>
         /// <param name="botAuthenticationKey1">Bot Account Authentication Key 1</param>
         /// <param name="botAuthenticationKey2">Bot Account Authentication Key 2</param>
-        public Bot(int botId, string botRegname, string botPassword, string botAuthenticationKey1, string botAuthenticationKey2)
+        public Bot(int botId, string botAuthenticationKey1, string botAuthenticationKey2, string botRegname, string botPassword)
         {
             this.Id = botId;
             this.Regname = botRegname;
@@ -55,7 +67,7 @@ namespace Botter
         /// <param name="botAvatarUrl">Bot Account Full Avatar URL (image only)</param>
         /// <param name="botHomepageUrl">Bot Account Full Homepage URL</param>
         /// <param name="botOwnerId">Bot Owner Account ID</param>
-        public Bot(int botId, string botRegname, string botPassword, string botAuthenticationKey1, string botAuthenticationKey2, string botDisplayName, string botAvatarUrl, string botHomepageUrl, int botOwnerId)
+        public Bot(int botId, string botAuthenticationKey1, string botAuthenticationKey2, string botRegname, string botPassword, string botDisplayName, string botAvatarUrl, string botHomepageUrl, int botOwnerId)
         {
             this.Id = botId;
             this.Regname = botRegname;
@@ -68,6 +80,9 @@ namespace Botter
             this.OwnerId = botOwnerId;
         }
 
+        /// <summary>
+        /// Store Bot Account Authentication Keys
+        /// </summary>
         public abstract class BotAuthenticationKeys
         {
             public string Key1 { get; set; }
